f3fd7d9ad4efb2d813e6e7d39e6322285844721d
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-10-29 15:23:38 -0300

    created an UnitTest class

---
+++ UnitTests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class UnitTests(unittest.TestCase): 
+    def test_true(self):
+        foo = False
+        self.assertTrue(foo)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
