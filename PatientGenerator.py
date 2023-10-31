d8511aca0b62a55fe59afa859c97001c941a0fc1
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-10-31 13:46:03 -0300

    simplified name logic

--- PatientGenerator.py
+++ PatientGenerator.py
@@ -1,11 +1,19 @@
 from Patient import Patient
-from PatientNames import patient_names
 import random
 
 def patient_generator(number_of_patients: int):
+    patient_names = [
+    "Luca Rossi",
+    "Sofia Bianchi",
+    "Marco Russo",
+    "Elena Martini",
+    "Alessio Conti",
+    "Valentina Ferrari",
+    "Giovanni Romano",
+    ]
     patients = []
     for i in range(0, number_of_patients, 1):
-        name_random = patient_names[random.randint(0, 99)]
+        name_random = patient_names[random.randint(0, 6)]
         age_random = random.randint(1, 110)
         priority = random.randint(1, 10)
         patient = Patient(name_random, age_random, priority)
