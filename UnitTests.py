08e2faf84c5c5d1368e423154520c27ff36e88d5
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-10-31 13:44:08 -0300

    refactored tests and implemented random generator of patients

--- UnitTests.py
+++ UnitTests.py
@@ -1,20 +1,30 @@
 import unittest
 from Patient import Patient
+from PatientGenerator import patient_generator
+import random
+
 class PatientTests(unittest.TestCase): 
     def setUp(self):
-        self._defaultPatient = Patient("Diego Trevisan", 22, 10)
+        self.patients: list = patient_generator(10)
 
-    def test_patient_name(self):
-        self.assertEqual("Diego Trevisan", self._defaultPatient.name)
+    def test_patient_generator_size(self):
+        self.assertEqual(10, len(self.patients))
+
+    def test_patient_instance_type(self):
+        random_patient = self.patients[random.randint(0, 9)]
+        self.assertIsInstance(random_patient, Patient)
     
-    def test_patient_age(self):
-        self.assertEqual(22, self._defaultPatient.age)
+    def test_patient_name(self):
+        random_patient_name = self.patients[random.randint(0, 9)].name
+        self.assertIsInstance(random_patient_name, str)
 
     def test_patient_priority(self):
-        self.assertEqual(10, self._defaultPatient.priority)
+        random_patient_priority = self.patients[random.randint(0, 9)].priority
+        self.assertIsInstance(random_patient_priority, int)
     
-
-
+    def test_patient_age(self):
+        random_patient_age = self.patients[random.randint(0, 9)].age
+        self.assertIsInstance(random_patient_age, int)
 
 if __name__ == "__main__":
     unittest.main()
